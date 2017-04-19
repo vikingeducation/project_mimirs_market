1e5df8bae83ee1f806c021bececee0f2bd13d3ab
Egle Libby <eglelibby@gmail.com>
2017-04-19 12:39:26 -0400

    show page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-var url = require('url');
-const express = require('express');
+var url = require("url");
+const express = require("express");
 let router = express.Router();
-var models = require('./../models/sequelize');
+var models = require("./../models/sequelize");
 var Product = models.Product;
 var Category = models.Category;
 var sequelize = models.sequelize;
@@ -16,11 +16,34 @@ var onIndex = (req, res) => {
     products = product;
     Category.findAll().then(category => {
       categories = category;
-      res.render('products/index', { products, categories });
+      res.render("products/index", { products, categories });
     });
   });
 };
 
-router.get('/', onIndex);
+var onShow = (req, res) => {
+  var products, currentProduct;
+
+  Product.findById(req.params.id, {
+    include: [{ model: Category, required: true }]
+  }).then(product => {
+    currentProduct = product;
+    Product.findAll(
+      {
+        where: { categoryId: currentProduct.categoryId }
+      },
+      {
+        limit: 30
+      }
+    ).then(result => {
+      products = result;
+      res.render("products/show", { products, currentProduct });
+    });
+  });
+};
+
+router.get("/", onIndex);
+
+router.get("/products/:id", onShow);
 
 module.exports = router;
