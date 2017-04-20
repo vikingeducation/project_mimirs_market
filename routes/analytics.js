4d9c59dd1cd27bf34cc238d3c363584c64019614
Egle Libby <eglelibby@gmail.com>
2017-04-20 17:28:22 -0400

    more queries

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,56 +1,30 @@
-var url = require('url');
-const express = require('express');
+var url = require("url");
+const express = require("express");
 let router = express.Router();
-var modelsSeq = require('./../models/sequelize');
+var modelsSeq = require("./../models/sequelize");
 var Product = modelsSeq.Product;
 var Category = modelsSeq.Category;
 var sequelize = modelsSeq.sequelize;
-var mongoose = require('mongoose');
-var modelsMon = require('./../models/mongoose');
-var Order = mongoose.model('Order');
+var mongoose = require("mongoose");
+var modelsMon = require("./../models/mongoose");
+var Order = mongoose.model("Order");
+var dbInfo = require("./../lib/dbInfo");
 
-router.get('/', (req, res) => {
-  let totals = Order.aggregate(
-    // Limit to relevant documents and potentially take advantage of an index
-    [
-      {
-        $group: {
-          _id: null,
-          totalRevenue: { $sum: '$charge.amount' },
-          totalUnitsEver: { $sum: '$totalUnits' },
-          totalTransaction: { $sum: 1 }
-        }
-      }
-    ]
-  );
-
-  let usersTotal = Order.aggregate(
-    { $group: { _id: '$email' } },
-    { $group: { _id: 1, totalUsers: { $sum: 1 } } },
-    { $project: { _id: 0 } }
-  );
-
-  let productTotal = Product.count({});
-
-  let categoryTotal = Category.count({});
-
-  let statesTotal = Order.aggregate(
-    { $group: { _id: '$state' } },
-    { $group: { _id: 1, totalStates: { $sum: 1 } } },
-    { $project: { _id: 0 } }
-  );
-
-  let promiseArr = [
-    totals,
-    usersTotal,
-    productTotal,
-    categoryTotal,
-    statesTotal
-  ];
+router.get("/", (req, res) => {
+  let promiseArr = dbInfo.getTotals();
+  promiseArr.push(dbInfo.statesRevenue());
   Promise.all(promiseArr).then(result => {
-    console.log('RESULT', result);
-
-    res.render('analytics/index', {});
+    console.log("RESULT", result);
+    let totals = {
+      totalRevenue: result[0][0].totalRevenue,
+      totalUnitsEver: result[0][0].totalUnitsEver,
+      totalTransactions: result[0][0].totalTransactions,
+      totalUsers: result[1][0].totalUsers,
+      totalProducts: result[2],
+      totalCategories: result[3],
+      totalStates: result[4][0].totalStates
+    };
+    res.render("analytics/index", { totals });
   });
 });
 
