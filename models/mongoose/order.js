ee315fe4292d2d03cbddd56b454eb9c9f9670ad6
Egle Libby <eglelibby@gmail.com>
2017-04-20 13:04:38 -0400

    fixed mongoose model

--- models/mongoose/order.js
+++ models/mongoose/order.js
@@ -1,10 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var OrderSchema = new Schema({
-  charge: Schema.Types.Mixed
-});
+var OrderSchema = new Schema(
+  {
+    charge: Schema.Types.Mixed,
+    firstName: String,
+    lastName: String,
+    email: String,
+    street: String,
+    city: String,
+    state: String,
+    shoppingCart: Array
+  },
+  {
+    timestamps: true
+  }
+);
 
-var Order = mongoose.model('Order', OrderSchema);
+var Order = mongoose.model("Order", OrderSchema);
 
 module.exports = Order;
--- routes/cart.js
+++ routes/cart.js
@@ -1,13 +1,13 @@
-var url = require('url');
-const express = require('express');
+var url = require("url");
+const express = require("express");
 let router = express.Router();
-var modelsSeq = require('./../models/sequelize');
+var modelsSeq = require("./../models/sequelize");
 var Product = modelsSeq.Product;
 var Category = modelsSeq.Category;
 var sequelize = modelsSeq.sequelize;
-var mongoose = require('mongoose');
-var modelsMon = require('./../models/mongoose');
-var Order = mongoose.model('Order');
+var mongoose = require("mongoose");
+var modelsMon = require("./../models/mongoose");
+var Order = mongoose.model("Order");
 
 // ----------------------------------------
 // STRIPE
@@ -16,7 +16,7 @@ var {
   STRIPE_SK,
   STRIPE_PK
 } = process.env;
-var stripe = require('stripe')(STRIPE_SK);
+var stripe = require("stripe")(STRIPE_SK);
 
 var totalAmount = function(cartProducts) {
   var total = 0;
@@ -26,13 +26,13 @@ var totalAmount = function(cartProducts) {
   return total;
 };
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   var cartProducts = req.session.shoppingCart;
   var total = totalAmount(cartProducts);
-  res.render('cart/index', { cartProducts, total });
+  res.render("cart/index", { cartProducts, total });
 });
 
-router.post('/updateQuantity', (req, res) => {
+router.post("/updateQuantity", (req, res) => {
   var quantity = req.body.productQuantity;
   var productId = Number(req.body.productId);
   var shoppingCart = req.session.shoppingCart;
@@ -44,10 +44,10 @@ router.post('/updateQuantity', (req, res) => {
   });
 
   req.session.shoppingCart = shoppingCart;
-  res.redirect('back');
+  res.redirect("back");
 });
 
-router.post('/remove', (req, res) => {
+router.post("/remove", (req, res) => {
   var productId = Number(req.body.productId);
   var shoppingCart = req.session.shoppingCart;
   var indexOfRemoval;
@@ -60,22 +60,22 @@ router.post('/remove', (req, res) => {
 
   shoppingCart.splice(indexOfRemoval, 1);
   req.session.shoppingCart = shoppingCart;
-  res.redirect('back');
+  res.redirect("back");
 });
 
-router.post('/clear', (req, res) => {
+router.post("/clear", (req, res) => {
   req.session.shoppingCart = [];
-  res.redirect('back');
+  res.redirect("back");
 });
 
-router.get('/checkout', (req, res) => {
+router.get("/checkout", (req, res) => {
   var cartProducts = req.session.shoppingCart;
   var total = totalAmount(cartProducts);
   //create description based off of products in cart to send to render?
-  res.render('cart/checkout', { cartProducts, STRIPE_PK, total });
+  res.render("cart/checkout", { cartProducts, STRIPE_PK, total });
 });
 
-router.post('/charges', (req, res) => {
+router.post("/charges", (req, res) => {
   var charge = req.body;
   var shoppingCart = req.session.shoppingCart;
   var total = charge.amount;
@@ -89,26 +89,28 @@ router.post('/charges', (req, res) => {
   stripe.charges
     .create({
       amount: Number(total),
-      currency: 'usd',
-      description: 'something',
+      currency: "usd",
+      description: "something",
       source: charge.stripeToken
     })
     .then(charge => {
-      charge.firstName = firstName;
-      charge.lastName = lastName;
-      charge.street = street;
-      charge.city = city;
-      charge.state = state;
-
-      charge.shoppingCart = shoppingCart;
-
-      var newOrder = new Order(charge);
+      var email = charge.name;
+      var newOrder = new Order({
+        charge,
+        firstName,
+        lastName,
+        email,
+        street,
+        city,
+        state,
+        shoppingCart
+      });
 
       newOrder.save();
     })
     .then(() => {
       req.session.shoppingCart = [];
-      res.redirect('/cart');
+      res.redirect("/cart");
     });
   //.catch(() => res.status(500).send(e.stack));
 });
