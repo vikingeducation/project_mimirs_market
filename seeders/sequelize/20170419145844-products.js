dac18c20e28e2d8607d2b89f8d6a8ce857885bf0
Egle Libby <eglelibby@gmail.com>
2017-04-21 16:10:20 -0400

    fix seed file

--- seeders/sequelize/20170419145844-products.js
+++ seeders/sequelize/20170419145844-products.js
@@ -1,23 +1,23 @@
-'use strict';
-var faker = require('faker');
+"use strict";
+var faker = require("faker");
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
     var products = [];
     var photos = [
-      'viking_guy.jpg',
-      'viking_girl.jpg',
-      'business.jpeg',
-      'cat.jpeg',
-      'food.jpeg',
-      'nature.jpeg',
-      'transport.jpeg'
+      "viking_guy.jpg",
+      "viking_girl.jpg",
+      "business.jpeg",
+      "cat.jpeg",
+      "food.jpeg",
+      "nature.jpeg",
+      "transport.jpeg"
     ];
 
     for (let i = 1; i < 100; i++) {
       let color = faker.commerce.color();
       color = color[0].toUpperCase() + color.substring(1);
-      let name = color + ' ' + faker.commerce.product();
+      let name = color + " " + faker.commerce.product();
       let sku = faker.finance.account();
 
       let adjective = faker.commerce.productAdjective();
@@ -26,12 +26,10 @@ module.exports = {
       let material = faker.commerce.productMaterial();
       material = material[0].toLowerCase() + material.substring(1);
 
-      let description = adjective + ' ' + material + ' ' + name;
+      let description = adjective + " " + material + " " + name;
       let price = faker.commerce.price();
       let photo = photos[Math.floor(Math.random() * 6) + 0];
       let categoryId = Math.floor(Math.random() * 20) + 1;
-
-      let unitsSold = 0;
 
       products.push({
         name,
@@ -39,14 +37,13 @@ module.exports = {
         description,
         price,
         photo,
-        categoryId,
-        unitsSold
+        categoryId
       });
     }
-    return queryInterface.bulkInsert('Products', products);
+    return queryInterface.bulkInsert("Products", products);
   },
 
   down: function(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Products', null, {}, models.Product);
+    return queryInterface.bulkDelete("Products", null, {}, models.Product);
   }
 };
