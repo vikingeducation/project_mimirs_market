b62eafe772ad16b36675434cbf70a5b0afc3f963
William Whitworth <william10charles@gmail.com>
2017-04-21 14:39:42 -0600

    adjusting config for heroku

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,27 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 const username = process.env.PG_USERNAME;
 const password = process.env.PG_PASSWORD;
 
 module.exports = {
-  "development": {
-    "username": username,
-    "password": password,
-    "database": "project_mimirs_market_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: username,
+    password: password,
+    database: "project_mimirs_market_development",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "test": {
-    "username": username,
-    "password": password,
-    "database": "project_mimirs_market_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: username,
+    password: password,
+    database: "project_mimirs_market_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
   }
 };
