a51c3c0929a2649f01cd7d0cec2cb85d9471914d
Christian Florez <florez.christian@gmail.com>
2017-06-15 12:51:02 -0400

    clean up files

--- routes/analytics.js
+++ routes/analytics.js
@@ -12,18 +12,6 @@ router.get('/', (req, res) => {
     .then(data => {
       res.render('analytics/index', { data } );
     });
-  // let totalRevenue;
-  // Order.aggregate([
-  //   {$match: {}}, 
-  //   {$group: { _id: null, amount: { $sum: "$revenue" } } }
-  // ])
-  //   .then(results => {
-  //     totalRevenue = results[0];
-  //     console.log(JSON.stringify(results, null, 2));
-  //     res.render('analytics/index', {
-  //       totalRevenue
-  //     });
-  //   });
 });
 
 module.exports = router;
