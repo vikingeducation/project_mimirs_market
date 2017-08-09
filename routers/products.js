43aabf7b4eea7be4e163eaa2e5db6db7a97965c9
Eric Glover <etgt28@mail.missouri.edu>
2017-08-09 17:32:30 -0500

    searching

--- routers/products.js
+++ routers/products.js
@@ -2,12 +2,14 @@ var express = require("express");
 var router = express.Router();
 const { Product } = require("../models/sequelize");
 
+//index Route
 router.get("/", (req, res) => {
   Product.findAll({}).then(products => {
     res.render("products/index", { products });
   });
 });
 
+//show Route
 router.get("/show/:productId", (req, res) => {
   Product.findById(req.params.productId).then(product => {
     Product.findAll({
@@ -17,6 +19,19 @@ router.get("/show/:productId", (req, res) => {
       console.log(relatedProducts);
       res.render("products/show", { product, relatedProducts });
     });
+  });
+});
+
+///search
+router.post("/search", (req, res) => {
+  let r = `${req.body.search}`;
+  Product.findAll({
+    where: {
+      name: { $regexp: r }
+    }
+  }).then(products => {
+    console.log(`products = ${products}`);
+    res.render("products/index", { products });
   });
 });
 
