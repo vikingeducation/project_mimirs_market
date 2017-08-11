691ce4b93f517fd640f5dcc92717d1adbe2543ad
Eric Glover <etgt28@mail.missouri.edu>
2017-08-11 10:46:11 -0500

    bug fixes

--- seeds/sequelize/20170809163150-products.js
+++ seeds/sequelize/20170809163150-products.js
@@ -21,46 +21,46 @@ const productPath = __dirname + "/../../" + productBasename;
 //     db[model.name] = model;
 //   });
 //seeds the categories
-let getUrls = async function() {
-  let productHash;
-  try {
-    productHash = await new Promise((resolve, reject) => {
-      cp.exec(`ls -R ${productPath}`, (err, out, stdErr) => {
-        //grab all the categories
-
-        //store them all in a hash
-        //return the hash for seeding
-        let strArr = out.split("\n");
-        const c = strArr.filter(item => {
-          return !(item.includes(".") || !item);
-        });
-        //then for each category grab it's files
-
-        c.forEach(category => {});
-        resolve(c);
-
-        console.log(`out = ${process.argv[1] + "/public/images"}`);
-        console.log(`out = ${out}`);
-      });
-    });
-  } catch (e) {
-    throw e;
-  }
-  return await productHash;
-};
+// let getUrls = async function() {
+//   let productHash;
+//   try {
+//     productHash = await new Promise((resolve, reject) => {
+//       cp.exec(`ls -R ${productPath}`, (err, out, stdErr) => {
+//         //grab all the categories
+//
+//         //store them all in a hash
+//         //return the hash for seeding
+//         let strArr = out.split("\n");
+//         const c = strArr.filter(item => {
+//           return !(item.includes(".") || !item);
+//         });
+//         //then for each category grab it's files
+//
+//         c.forEach(category => {});
+//         resolve(c);
+//
+//         console.log(`out = ${process.argv[1] + "/public/images"}`);
+//         console.log(`out = ${out}`);
+//       });
+//     });
+//   } catch (e) {
+//     throw e;
+//   }
+//   return await productHash;
+// };
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
     let seeds = [];
     // let urls = getUrls();
     for (var i = 0; i < 50; i++) {
-      let name = `${faker.random.catch_phrase_descriptor()} ${faker.random.catch_phrase_descriptor()} ${faker.random.bs_noun()}`;
+      let name = `${faker.hacker.adjective()} ${faker.hacker.noun()}`;
       let p = {
         name: name.trim(),
-        description: faker.Lorem.sentence().trim(),
-        price: faker.random.number(5000),
+        description: faker.lorem.sentence().trim(),
+        price: faker.random.number(500),
         sku: Math.random() * 122221,
-        img: "images/weapons/war_axe.jpg",
+        img: "images/products/weapons/war_axe.jpg",
         categoryId: i % 10 + 1
       };
       seeds.push(p);
