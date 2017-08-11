d16ef39ea6bf304edd8ba8136b648ee75de2f588
Eric Glover <etgt28@mail.missouri.edu>
2017-08-11 11:48:55 -0500

    checkout form mostly done

--- routers/checkout.js
+++ routers/checkout.js
@@ -1,13 +1,22 @@
 var express = require("express");
 var router = express.Router();
-const { Product, Category } = require("../models/sequelize");
+const { Product, Category, State } = require("../models/sequelize");
 
 //checkout route
 router.get("/", (req, res) => {
-  return res.render("cart/checkout", {
-    cart: req.session.cart,
-    total: getTotal(req.session.cart)
-  });
+  let states = getStates();
+  states
+    .then(stateList => {
+      return res.render("cart/checkout", {
+        cart: req.session.cart,
+        states: stateList,
+        total: getTotal(req.session.cart)
+      });
+    })
+    .catch(e => {
+      console.log(`TERRIBLE THINGS HAVE HAPPENED: ${e}`);
+      throw e;
+    });
 });
 
 module.exports = router;
@@ -17,4 +26,15 @@ function getTotal(cart) {
   return cart.reduce((sum, item) => {
     return sum + item.price * item.quantity;
   }, 0);
+}
+function getStates() {
+  return new Promise(resolve => {
+    State.findAll({}).then(states => {
+      return resolve(
+        states.map(model => {
+          return model.name;
+        })
+      );
+    });
+  });
 }
