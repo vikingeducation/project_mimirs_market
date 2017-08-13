afe0922fb4f4b1e0e0e7aae3696ed6f48c4690f3
Eric Glover <etgt28@mail.missouri.edu>
2017-08-12 19:52:40 -0500

    seeding local url imgs

--- seeds/sequelize/20170809163150-products.js
+++ seeds/sequelize/20170809163150-products.js
@@ -1,63 +1,83 @@
 "use strict";
 const model = require("../../models/sequelize");
+const cp = require("child_process");
+const fs = require("fs");
 let faker = require("Faker");
 
-//file path to products
-// const productBasename = require(__dirname + "/../../.env")["productPath"];
-// const productPath = __dirname + "/../../" + productBasename;
+//setup for getting needed file paths
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: "/Users/ericglover/Desktop/Programming/db/project_mimirs_market/.env"
+  });
+}
+var { PRODUCT_BASENAME } = process.env;
+const productPath = __dirname + "/../../" + PRODUCT_BASENAME;
 
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
-// var { PRODUCT_BASENAME } = process.env;
-// const productPath = __dirname + "/../../" + PRODUCT_BASENAME;
-
-//TODO: GRAB IMAGES AND SEED THEM
-//seeds the products
-////////store them all in a hash
-////////return the hash for seeding
-// let getUrls = function() {
-//   let productHash = {};
-//   let pCat = new Promise((resolve, reject) => {
-//     cp.exec(`ls -R ${productPath}`, (err, out, stdErr) => {
-//       //grab all the categories
-//       // console.log(`out = ${process.argv[1] + "/public/images"}`);
-//       console.log(`out = ${out}`);
-//
-//
-//       let strArr = out.split("\n");
-//       const c = strArr.filter(item => {
-//         return !(item.includes(".") || !item);
-//       });
-//       return resolve(c);
-//
-//
-//     });
-//   }).then(categories => {
-//     //for each category get the product img urls
-//     categories.forEach()
-//   });
-//
-//   return productHash;
-// };
+//get categories
+let getCategories = async function() {
+  let dirs;
+  try {
+    dirs = await new Promise((resolve, reject) => {
+      cp.exec(`ls ${productPath}`, (err, out, stdErr) => {
+        //ignore all the .txt files
+        //reject if empty or includes '.'
+        const c = out.split("\n").filter(item => {
+          return !(item.includes(".") || !item);
+        });
+        console.log(`c = `, c);
+        resolve(c);
+      });
+    });
+  } catch (e) {
+    throw e;
+  }
+  // console.log(`dirs = ${dirs}`);
+  return await dirs;
+  // return dirs;
+};
 
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    let seeds = [];
-    // let urls = getUrls();
-    for (var i = 0; i < 50; i++) {
-      let name = `${faker.hacker.adjective()} ${faker.hacker.noun()}`;
-      let p = {
-        name: name.trim(),
-        description: faker.lorem.sentence().trim(),
-        price: faker.random.number(500),
-        sku: Math.random() * 122221,
-        img: "images/products/weapons/war_axe.jpg",
-        categoryId: i % 10 + 1
-      };
-      seeds.push(p);
-    }
-    return queryInterface.bulkInsert("Products", seeds, {});
+  up: async function(queryInterface, Sequelize) {
+    //make a hash of category names and product url arrays
+    let productHash = {};
+    await getCategories().then(cats => {
+      let promises = cats.map(folder => {
+        productHash[folder] = [];
+        return new Promise((resolve, reject) => {
+          fs.readdir(`${productPath}/${folder}`, (err, files) => {
+            if (err) reject(err);
+            productHash[folder] = files;
+            return resolve(files);
+          });
+        });
+      });
+      Promise.all(promises)
+        .then(urls => {
+          console.log("productHash = ", productHash);
+          //make some products
+          let seeds = [];
+          Object.keys(productHash).forEach((key, index) => {
+            for (let i = 0; i < productHash[key].length; i++) {
+              let name = "bob";
+              let p = {
+                name: name.trim(),
+                description: "Awesome Things", //faker.lorem.sentence().trim(),
+                price: faker.random.number(500),
+                sku: Math.random() * 122221,
+                img: `images/products/${key}/${productHash[key][i]}`,
+                categoryId: index + 1
+              };
+              console.log("p =", p);
+              seeds.push(p);
+            }
+          });
+          console.log("seeds =", seeds);
+          return queryInterface.bulkInsert("Products", seeds, {});
+        })
+        .catch(e => {
+          throw e;
+        });
+    });
   },
 
   down: function(queryInterface, Sequelize) {
