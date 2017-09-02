aa5ce1fa68ba7d44dac6a44d76fdd88db9c01256
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-02 19:58:06 +0000

    order save working

--- routes/charges.js
+++ routes/charges.js
@@ -9,7 +9,6 @@ var Order = mongoose.model("Order");
 var OrderItem = mongoose.model("OrderItem");
 
 const makeCustomer = session => {
-  console.log(session);
   let customer = {
     fname: session.fname,
     lname: session.lname,
@@ -38,81 +37,34 @@ const makeItems = items => {
   return purchases;
 };
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const items = getCartInfo(req.session.cart);
-  console.log(req.session);
   const customer = makeCustomer(req.session);
   const purchases = makeItems(items.cartItems);
-  let orderItems = [];
+  let charge = req.body;
+  let thisCharge = await stripe.charges.create({
+    amount: items.total,
+    currency: "usd",
+    description: "test charge",
+    source: charge.stripeToken
+  });
   let brand;
   let total;
-  let order = new Order();
-  order.customer = customer;
-  order.orderItems = [];
-  console.log(order);
+  let orderItems = [];
+  let order = new Order({
+    customer
+  });
+  order.total = thisCharge.amount;
+  order.transactionID = thisCharge.balance_transaction;
+  order.cardType = thisCharge.source.brand;
   purchases.forEach(purchase => {
     let orderItem = new OrderItem(purchase);
     orderItems.push(orderItem.save());
   });
-  Promise.all(orderItems)
-    .then(results => {
-      results.forEach(result => {
-        order.orderItems.push(result._id);
-      });
-      return order.save();
-    })
-    .then(result => {
-      console.log(result);
-    });
-    .catch(e=> res.status(500).end(e.stack))
-  // stripe.charges
-  //   .create({
-  //     amount: items.total,
-  //     currency: "usd",
-  //     description: "test charge",
-  //     source: charge.stripeToken
-  //   })
-  //   .then(charge => {
-  //     brand = charge.source.brand;
-  //     total = charge.total;
-  //   });
-  // let orderItems = [];
-  // purchases.forEach(purchase => {
-  //   orderItem = new OrderItem(purchase);
-  //   orderItems.push(orderItem);
-  // });
-  // (order.cardType = brand), (order.total = total), (order.orderItems = orderItems);
-  // order.save();
-  // res.redirect("/");
+  let promisedItems = await Promise.all(orderItems);
+  order.orderItems = promisedItems;
+  let savedOrder = await order.save();
+  res.redirect("/");
 });
-
-//       return result;
-//     });
-//   let purchases = [];
-//   items.cartItems.forEach(item => {
-//     item = new OrderItem({
-//       id: item.id,
-//       name: item.name,
-//       price: item.price,
-//       sku: item.sku,
-//       description: item.description,
-//       category: item.category,
-//       quantity: item.quantity
-//     });
-//     purchases.push(item);
-//   });
-//   let order = new Order({
-//     // cardType: result.source.brand,
-//     // total: result.total,
-//     orderItems: purchases,
-//     fname: firstName,
-//     lname: lastName,
-//     email: custEmail,
-//     street: req.body.street,
-//     city: req.body.city,
-//     state: req.body.state
-//   });
-//   console.log(order);
-// });
 
 module.exports = router;
