443d156391ae090886d59c35d84f4f3dcdd937c6
Greg Filipczak <gregfilipczak@gmail.com>
2017-09-02 20:25:00 +0000

    sql

--- config/sequelize.js
+++ config/sequelize.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres"
   }
 };
