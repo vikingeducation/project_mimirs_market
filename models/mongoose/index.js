188b4175592dbbd104a65580194491542e5ad56c
Coelacanth7 <galford151@gmail.com>
2017-11-14 18:35:00 -0700

    set up basic structure

---
+++ models/mongoose/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
+
+mongoose.Promise = bluebird;
+
+const models = {};
+
+// Load models and attach to models here
+// models.User = require('./user');
+// models.Ratable = require('./ratable');
+// models.Hotel = require('./hotel');
+// models.Motel = require('./motel');
+// models.Rating = require('./rating');
+
+module.exports = models;
