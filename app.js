4e2d2f8c64984ff1a89e2d31fc41089e7b9c5118
EdwinYung <eyung7@gmail.com>
2017-12-07 12:11:16 -0500

    before merge

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // ----------------------------------------
 // App Variables
 // ----------------------------------------
-app.locals.appName = "My App";
+app.locals.appName = 'My App';
 
 // ----------------------------------------
 // ENV
 // ----------------------------------------
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 // ----------------------------------------
 // Body Parser
 // ----------------------------------------
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ----------------------------------------
 // Sessions/Cookies
 // ----------------------------------------
-const cookieSession = require("cookie-session");
+const cookieSession = require('cookie-session');
 
 app.use(
   cookieSession({
-    name: "session",
-    keys: [process.env.SESSION_SECRET || "secret"]
+    name: 'session',
+    keys: [process.env.SESSION_SECRET || 'secret'],
   })
 );
 
@@ -39,14 +39,14 @@ app.use((req, res, next) => {
 // ----------------------------------------
 // Flash Messages
 // ----------------------------------------
-const flash = require("express-flash-messages");
+const flash = require('express-flash-messages');
 app.use(flash());
 
 // ----------------------------------------
 // Method Override
 // ----------------------------------------
-const methodOverride = require("method-override");
-const getPostSupport = require("express-method-override-get-post-support");
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
 
 app.use(
   methodOverride(
@@ -59,7 +59,7 @@ app.use(
 // Referrer
 // ----------------------------------------
 app.use((req, res, next) => {
-  req.session.backUrl = req.header("Referer") || "/";
+  req.session.backUrl = req.header('Referer') || '/';
   next();
 });
 
@@ -71,42 +71,42 @@ app.use(express.static(`${__dirname}/public`));
 // ----------------------------------------
 // Logging
 // ----------------------------------------
-const morgan = require("morgan");
-const morganToolkit = require("morgan-toolkit")(morgan);
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan);
 
 app.use(morganToolkit());
 
 // ----------------------------------------
 // Routes
 // ----------------------------------------
-app.use("/", (req, res) => {
-  req.flash("Hi!");
-  res.render("welcome/index");
+app.use('/', (req, res) => {
+  req.flash('Hi!');
+  res.render('welcome/index');
 });
 
 // ----------------------------------------
 // Template Engine
 // ----------------------------------------
-const expressHandlebars = require("express-handlebars");
-const helpers = require("./helpers");
+const expressHandlebars = require('express-handlebars');
+const helpers = require('./helpers');
 
 const hbs = expressHandlebars.create({
   helpers: helpers,
-  partialsDir: "views/",
-  defaultLayout: "application"
+  partialsDir: 'views/',
+  defaultLayout: 'application',
 });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // ----------------------------------------
 // Server
 // ----------------------------------------
 const port = process.env.PORT || process.argv[2] || 3000;
-const host = "localhost";
+const host = 'localhost';
 
 let args;
-process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+process.env.NODE_ENV === 'production' ? (args = [port]) : (args = [port, host]);
 
 args.push(() => {
   console.log(`Listening: http://${host}:${port}\n`);
@@ -127,7 +127,7 @@ app.use((err, req, res, next) => {
   if (err.stack) {
     err = err.stack;
   }
-  res.status(500).render("errors/500", { error: err });
+  res.status(500).render('errors/500', { error: err });
 });
 
 module.exports = app;
