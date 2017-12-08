df9e382d0482f9bd0e9b2c4090f02c29419fe414
EdwinYung <eyung7@gmail.com>
2017-12-08 08:43:57 -0500

    fixed categories

--- routers/products.js
+++ routers/products.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const router = express.Router();
 const {
   Product,
   sequelize,
   Category,
-  Sequelize: { Op }
-} = require("../models/sequelize");
-const h = require("../helpers");
+  Sequelize: { Op },
+} = require('../models/sequelize');
+const h = require('../helpers');
 
 // ----------------------------------------
 // Build Query Settings
@@ -19,40 +19,32 @@ const _buildSearchQuery = req => {
     return {};
   }
 
-  const queryObj = { price: req.query.price, categories: req.query.categories };
-
-
   return {
-    // [Op.and]: [
-      price: { [Op.between]: [+queryObj.price.min, +queryObj.price.max] },
-      // {Category: queryObj.categories.submission}
-            Category:queryObj.categories.submission
-    // ]
-
+    price: { [Op.between]: [+req.query.price.min, +req.query.price.max] },
+    Category: req.query.categories.submission,
+  };
 };
-}
-//
-// Product.find(include:Category, where:{Category.name: '1'}).then(lg);
-// Product.find({include:Category}).then(lg);
 
 // ----------------------------------------
 // Index
 // ----------------------------------------
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     //query is an object that has the search queries
     const query = _buildSearchQuery(req);
-    console.log("QUERY");
-    console.log(query);
-    const products = await Product.findAll({
-      // include: Category,
-      where: query.price,
-      include: [{model:Category, where: { "name":query.Category }}]
+    let queryObj = {};
+    queryObj['price'] = query.price;
+    let queryObjCategory = {};
+    queryObjCategory['name'] = query.Category;
+
+    let products = await Product.findAll({
+      where: queryObj,
+      include: [{ model: Category, where: queryObjCategory }],
     });
 
-    const categories = await Category.findAll({});
-
-    res.render("products/index", { products, categories });
+    let categories = await Category.findAll({});
+  
+    res.render('products/index', { products, categories });
   } catch (e) {
     next(e);
   }
@@ -61,17 +53,17 @@ router.get("/", async (req, res, next) => {
 // ----------------------------------------
 // Show
 // ----------------------------------------
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findById(req.params.id, {
-      include: Category
+      include: Category,
     });
     console.log(product);
     if (!product) {
-      req.flash("error", "Product not found");
-      return res.redirect("/products");
+      req.flash('error', 'Product not found');
+      return res.redirect('/products');
     }
-    res.render("products/show", { product });
+    res.render('products/show', { product });
   } catch (e) {
     next(e);
   }
