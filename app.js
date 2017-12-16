ca1be11bfc7ce9aeafee81aa68b5016ac5e0a1c0
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-12-16 17:34:34 +0100

    basic setup

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+
+// Body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Session/cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const cookieSession = require('cookie-session');
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['asdf1234']
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  // Cart middleware here
+  next();
+});
+
+// Flash messages
+const flash = require('express-flash-messages');
+app.use(flash());
+
+// Method override
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
+app.use(methodOverride(getPostSupport.callback, getPostSupport.options));
+
+// Public
+app.use(express.static(`${__dirname}/public`));
+
+// Logging
+const morgan = require('morgan');
+app.use(morgan('tiny'));
+
+// Mongoose
+const mongoose = require('mongoose');
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState) {
+    next();
+  } else {
+    require('./mongo')().then(() => next());
+  }
+});
+
+// Template engine
+const expressHandlebars = require('express-handlebars');
+const helpers = require('./helpers');
+
+const hbs = expressHandlebars.create({
+  partialsDir: 'views/',
+  defaultLayout: 'application',
+  helpers: helpers.registered
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Routes
+
+// Server
+const port = process.env.PORT || process.argv[2] || 3000;
+const host = 'localhost';
+
+let args;
+process.env.NODE_ENV === 'production' ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}`);
+});
+
+app.listen.apply(app, args);
+
+module.exports = app;
---
+++ seeds/sequelize/20171216150527-categories.js
@@ -0,0 +1,30 @@
+'use strict';
+const models = require('../../models/sequelize');
+const faker = require('faker');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Categories', categories);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Categories', null, {}, models.Category);
+  }
+};
